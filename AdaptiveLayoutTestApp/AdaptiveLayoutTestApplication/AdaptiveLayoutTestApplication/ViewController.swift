0d39be704b2981ec4c704e08d166f63681ab4dc8
Mihail Borisov <mihail.borisov@binomo.com>
2019-10-03 17:48:01 +0300

    Init repo

---
+++ AdaptiveLayoutTestApp/AdaptiveLayoutTestApplication/AdaptiveLayoutTestApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptiveLayoutTestApplication
+//
+//  Created by M.Borisov on 02/10/2019.
+//  Copyright © 2019 M.Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
